df73ecaca1d94d696073a01b9b17e6f2d819b37e
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-06-20 02:03:22 +0530

    Add files via upload

---
+++ day 2 .py
@@ -0,0 +1,7 @@
+# comments, Escape Sequences and print statements
+
+print("Hey I am a good boy and this\n"
+      "viewer is also a good boy")
+
+# by using \n we can print in new line
+# by using ctrl + / we can comment multiple lines at a time
