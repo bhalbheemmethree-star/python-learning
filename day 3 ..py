0f564c51eaafa48de77c8cdfe9b425089b852ac4
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-06-21 00:06:40 +0530

    Add files via upload

---
+++ day 3 ..py
@@ -0,0 +1,31 @@
+# variables and data types
+
+a = 1
+print(a)
+b = "bhalbheem"
+print(b)
+
+# string should always be in quotes
+# we can use variable operations only if they are of the same data type
+
+print(type(a))
+print(type(b))
+
+# numeric data types: int, float, complex
+int = 3 , 7
+float = 3.14 , 2.71
+complex = 2 + 3j , 4 - 5j
+
+# text data type: str
+str = "hello" , 'world' , "python"
+
+# boolean data 
+bool = True , False
+
+# sequence data types: list, tuple
+list = [1, 2, 3, "hello", True] # MUTABLE
+tuple = (1, 2, 3, "hello", True) # IMMUTABLE
+
+# dictionary data type: dict
+dict = {"name": "bhalbheem", "age": 20, "city": "pune"} # MUTABLE and only key-value pairs are allowed
+
