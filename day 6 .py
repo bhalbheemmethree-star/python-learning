466a8a1a7b5d95ebed23c8b3fb78b5e4413e4f35
BHAL BHEEM METHREE <bhalbheemmethree@gmail.com>
2026-06-24 00:18:19 +0530

    Add files via upload

---
+++ day 6 .py
@@ -0,0 +1,17 @@
+# strings in python 
+# any thing enclosed in single or double quotes is a string in python
+
+name = "bhal bheem" # this is a string variable
+print("Hello ," + name) 
+
+# to add double quotes in a string we can use single quotes to enclose the string and vice versa
+
+print('my name is "bhal bheem"') # this will print my name is "bhal bheem"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+
+for character in name:
+    print (character) #this is for looping through the string and printing each character on a new line
