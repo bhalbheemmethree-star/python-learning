84abccf8c6a288c8a134dcd291d377dcba71a149
BHAL BHEEM METHREE <bhalbheemmethree@gmail.com>
2026-06-25 00:08:51 +0530

    Add files via upload

---
+++ day 7 .py
@@ -0,0 +1,14 @@
+# string slicing and operations in python
+
+names = "Bhalbheem,Rohit"
+print(names[0:9]) # this will print bhalbheem
+print(names[1:9]) # this will print halbheem
+print(names[ :9])
+print(names[0:12])
+print(len(names))
+
+Fruit = "Mango"
+len1 = len(Fruit)
+print("Mango is a",len1,"letter word")
+print(Fruit[0:-2])
+print(Fruit[-4:-2])
