1ea9fd2a76490aee01f58dcffd245eb0fb46eebf
BHAL BHEEM METHREE <bhalbheemmethree@gmail.com>
2026-07-01 11:46:49 +0530

    variables and data types completed
    Updated comments and variable assignments for clarity. Added examples of various data types in Python.

--- day03.py
+++ day03.py
@@ -1,32 +1,39 @@
+# day 03: Data types and Variables
+  •variables in python
+  •data types 
+  •Dynamic typing
+  •type() function
 
-# boolean data 
-# dictionary data type: dict
-# numeric data types: int, float, complex
-# sequence data types: list, tuple
-# string should always be in quotes
-# text data type: str
-# variables and data types
-# we can use variable operations only if they are of the same data type
+•
 a = 1
 b = "bhalbheem"
+c = 12.8
+
+•
+# text data type: str
+str = "hello" , 'world' , "python"
+
+# boolean data
 bool = True , False
 
-complex = 2 + 3j , 4 - 5j
-
+# dictionary data type: dict
 dict = {"name": "bhalbheem", "age": 20, "city": "pune"} # MUTABLE and only key-value pairs are  allowed
 
-float = 3.14 , 2.2
-
+# numeric data types: int, float, complex
 int = 3 , 7
+float = 3.14 , 2.2
+complex = 2 + 3j , 4 - 5j
 
+# sequence data types: list, tuple
 list = [1, 2, 3, "hello", True] # MUTABLE
+tuple = (1, 2, 3, "hello", True) # IMMUTABLE
 
-print(a) 
-print(b)
+# string should always be in quotes
+# we can use variable operations only if they are of the same data type
 
+•
 print(type(a))
 print(type(b))
+print(type(c)) 
 
-str = "hello" , 'world' , "python"
-tuple = (1, 2, 3, "hello", True) # IMMUTABLE
- 
+
