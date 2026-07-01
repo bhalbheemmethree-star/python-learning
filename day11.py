65139a8684b33f42792e1427edc56f0b1ee2950a
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-07-01 21:31:56 +0530

    for loops in python

--- day11.py
+++ day11.py
@@ -1,1 +1,22 @@
 # For loops in python 
+
+name = "bhalbheem"
+for i in name:
+    print(i)
+
+for i in name:
+    print(i, end=",")
+
+
+colours = ["red", "green", "blue", "yellow"]
+for color in colours:
+    print(color)
+    for i in color:
+        print(i)
+
+
+for x in range(5):    # this prints 1 to 4 
+    print(x)
+
+for x in range(5):
+    print(x + 1)     # this prints 1 to 5
