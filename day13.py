3c25a735e51f1b3eb2bd77da9b5e37e538f24ed1
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-07-05 16:14:27 +0530

    break and continue in python

--- day13.py
+++ day13.py
@@ -1,1 +1,29 @@
-# break 
+# # break and continue in python
+
+# for i in range(14):
+#     if i == 10:
+#         print("breaking the loop")
+#         break
+#     print("5 X",i+1,"=",5*(i+1))
+
+
+
+
+# for i in range(14):
+#     if i == 10:
+#         print("skip the iteration")
+#         continue
+#     print("5 X",i,"=",5*i)
+i = 6
+while True:
+    print(i)
+    i = i + 1
+    if(i%50 == 0):
+        break
+
+
+
+
+
+
+
