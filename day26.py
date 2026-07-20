36596655c827b91f14bf8456191a5343ff9b2a47
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-07-21 01:27:33 +0530

    Dictionary methods in python

---
+++ day26.py
@@ -0,0 +1,13 @@
+# Dictionary methods in python
+
+ep1 = {100: 68 , 101: 69 , 102: 90 , 103: 56 , 104: 78}
+ep2 = {105: 67 , 106: 69 , 107: 56}
+
+# ep1.update(ep2) # updates specific by adding 2 dicts 
+# ep1.clear() # clear all elements from dict and return empty dict
+# ep1.pop(101) # removes specific key value pair
+# ep1.popitem() # removes only last key value pair of a dict
+# del ep1 # deletes and entire dictionary
+# del ep1[100] # deletes specific key value pair
+
+print(ep1)
