fea82c1b24384bef8c843b275f4bb62a141afee8
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-07-26 21:48:25 +0530

    virtual environment and its commands in python

--- day33.py
+++ day33.py
@@ -1,1 +1,27 @@
 # Virtual environment
+
+# suppose i created a folder named venv now i can create virtual env file by command
+
+
+# create a virtual enviroment
+# python3 -m evnv myenv 
+
+# to activate that file i created 
+# source myenv/bin/activate --1 
+# pip3 install pandas -- 2 
+# to install python library use command 2 after installing command python3 and import pandas
+# if done just command deactivate
+
+# to create new file in venv
+# touch main.py
+
+# to return all pakages installed 
+# pip freeze
+
+# to create file of installed pakages
+# pip freeze > requirements.txt
+
+# to install all pakages are in file 
+# pip install -r requirements.txt
+
+# here requirements.txt is a file where all pakages installed 
